eb2d24027c62145332c3e0c93294fa3a81218a26
LucaBerardi6 <luca.berardi6@studio.unibo.it>
2022-03-14 13:07:27 +0100

    14/03

--- it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/mock/SonarMock.java
+++ it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/mock/SonarMock.java
@@ -1,32 +1,62 @@
 package it.unibo.radarSystem22.domain.mock;
 
+import it.unibo.radarSystem22.domain.Distance;
 import it.unibo.radarSystem22.domain.interfaces.IDistance;
 import it.unibo.radarSystem22.domain.interfaces.ISonar;
 
 public class SonarMock implements ISonar {
 
+	private boolean active;
+	private int distance;
+	
+	public SonarMock() {
+		this.active=false;
+		this.distance=90;
+	}
+	
 	@Override
 	public void activate() {
 		
-
+		active=true;
+		
+		new Thread() {
+			public void run() {
+				while( active && distance>= 0 ) {
+					
+					distance--;
+					
+					try {
+						Thread.sleep(250);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					
+						
+				}
+			
+		    }
+		}.start();
 	}
 
 	@Override
 	public void deactivate() {
 		
+		active=false;
 
 	}
 
 	@Override
 	public IDistance getDistance() {
 		
-		return null;
+		return new Distance(distance);
 	}
 
 	@Override
 	public boolean isActive() {
 		
-		return false;
+		return active;
 	}
 
 }
--- it.unibo.radarSystem22.domain/test/it/unibo/radarSystem22/domain/TestSonar.java
+++ it.unibo.radarSystem22.domain/test/it/unibo/radarSystem22/domain/TestSonar.java
@@ -2,13 +2,106 @@ package it.unibo.radarSystem22.domain;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import it.unibo.radarSystem22.domain.interfaces.ISonar;
+import it.unibo.radarSystem22.domain.mock.SonarMock;
 
 public class TestSonar {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
+	@Before
+	public void up() {
+		System.out.println("up test Sonar");
+	}
+	
+	@After
+	public void down() {
+		System.out.println("down test Sonar");		
+	}	
+	
+	@Test 
+	public void testSonarMockOnOff() {
+		
+		System.out.println("test Sonar Mock on");
+		ISonar sonar= new SonarMock();
+		
+		assertTrue( ! sonar.isActive() );
+		
+		sonar.activate();
+	
+		assertTrue( sonar.isActive() );
+		
+		sonar.deactivate();
+		
+		assertTrue( ! sonar.isActive() );
+		
+ 
+	
+	}	
+	
+	@Test 
+	public void testSonarMockDistance() throws InterruptedException {
+		
+		System.out.println("test Sonar Mock Distance");
+		ISonar sonar= new SonarMock();
+	
+		
+	
+		assertTrue( sonar.getDistance().getVal()==90 );
+		
+		
+		sonar.activate();
+		//
+		Thread.sleep(250);
+		assertTrue( sonar.getDistance().getVal()==89 );
+		//
+		Thread.sleep(1000);
+		assertTrue( sonar.getDistance().getVal()==85 );
+		//
+		Thread.sleep(1000);
+		assertTrue( sonar.getDistance().getVal()==81 );
+		//
+		sonar.deactivate();
+		assertTrue( sonar.getDistance().getVal()==81 );
+		assertTrue( ! sonar.isActive() );
+		
+ 
+	
+	}	
+	
+	/*@Test 
+	public void testSonarMockDistanceError() throws InterruptedException {
+		
+		System.out.println("test Sonar Mock error");
+		ISonar sonar= new SonarMock();
+	
+		
+	
+		assertTrue( sonar.getDistance().getVal()==90 );
+		
+		
+		sonar.activate();
+		//
+		Thread.sleep(250);
+		assertTrue( sonar.getDistance().getVal()==90 );
+		//
+		Thread.sleep(1000);
+		assertTrue( sonar.getDistance().getVal()==86 );
+		//
+		Thread.sleep(1000);
+		assertTrue( sonar.getDistance().getVal()==80 );
+		//
+		sonar.deactivate();
+		assertTrue( sonar.getDistance().getVal()==81 );
+		assertTrue( ! sonar.isActive() );
+		
+ 
+	
+	}	*/
+	
+	
+	
 }
