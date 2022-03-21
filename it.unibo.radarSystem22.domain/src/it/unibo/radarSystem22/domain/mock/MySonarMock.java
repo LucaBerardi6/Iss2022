e90b86302417be701f78f6c810b80b756e48db20
LucaBerardi6 <luca.berardi6@studio.unibo.it>
2022-03-21 11:58:22 +0100

    sprint2

---
+++ it.unibo.radarSystem22.domain/src/it/unibo/radarSystem22/domain/mock/MySonarMock.java
@@ -0,0 +1,62 @@
+package it.unibo.radarSystem22.domain.mock;
+
+import it.unibo.radarSystem22.domain.Distance;
+import it.unibo.radarSystem22.domain.interfaces.IDistance;
+import it.unibo.radarSystem22.domain.interfaces.ISonar;
+
+public class MySonarMock implements ISonar {
+
+	private boolean active;
+	private int distance;
+	
+	public MySonarMock() {
+		this.active=false;
+		this.distance=90;
+	}
+	
+	@Override
+	public void activate() {
+		
+		active=true;
+		
+		new Thread() {
+			public void run() {
+				while( active && distance>= 0 ) {
+					
+					distance--;
+					
+					try {
+						Thread.sleep(250);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					
+						
+				}
+			
+		    }
+		}.start();
+	}
+
+	@Override
+	public void deactivate() {
+		
+		active=false;
+
+	}
+
+	@Override
+	public IDistance getDistance() {
+		
+		return new Distance(distance);
+	}
+
+	@Override
+	public boolean isActive() {
+		
+		return active;
+	}
+
+}
